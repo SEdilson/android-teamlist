52290016527dae85aef1ad28d8eb5794e96281bf
SEdilson <edilson.silva00@hotmail.com>
2020-01-29 00:36:02 -0300

    Adding code of splash screen

---
+++ app/src/main/java/com/example/teamlist/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.example.teamlist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.teamlist.ui.activity.TeamListActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handleSplashScreen = new Handler();
+        handleSplashScreen.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initSplashActivity();
+            }
+        }, 2000);
+    }
+
+    private void initSplashActivity() {
+        Intent intent = new Intent(SplashScreenActivity.this, TeamListActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
